ad7651513302cf3eb50f96a649763b0f2fdfa12f
KOTARO SUTO <ng1a5ss43@gmail.com>
2020-07-26 05:30:42 +0900

    Add progress view referenced from https://www.simpleswiftguide.com/how-to-build-a-circular-progress-bar-in-swiftui/

--- PomodoroApp/ContentView.swift
+++ PomodoroApp/ContentView.swift
@@ -9,8 +9,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var progressValue: Float = 0.0
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color.yellow
+                .opacity(0.1)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                ProgressBar(progress: self.$progressValue)
+                    .frame(width: 150.0, height: 150.0)
+                    .padding(40.0)
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ProgressBar: View {
+    @Binding var progress: Float
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 20.0)
+                .opacity(0.3)
+                .foregroundColor(Color.red)
+            
+            Circle()
+                .trim(from: 0.0, to:min( CGFloat(self.progress), 1.0))
+                .stroke(style: StrokeStyle(lineWidth: 20.0, lineCap: .round, lineJoin: .round))
+                .foregroundColor(Color.red)
+                .rotationEffect(Angle(degrees: 270.0))
+                .animation(.linear)
+            
+            Text(String(format: "%.0f %%", min(self.progress, 1.0)*100.0))
+                .font(.largeTitle)
+                .bold()
+        }
     }
 }
 
