34133ffd38ff013a329e0b07b96ad876cffa3ed8
KOTARO SUTO <ng1a5ss43@gmail.com>
2020-07-26 05:13:26 +0900

    Initial Commit

---
+++ PomodoroApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PomodoroApp
+//
+//  Created by Kotaro Suto on 2020/07/26.
+//  Copyright © 2020 Kotaro Suto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
