52db0a460eb1c44e315dd3022f7ce064a62454fc
KOTARO SUTO <ng1a5ss43@gmail.com>
2020-11-20 06:14:37 +0900

    Run 'pod update' & set up Admob

--- PomodoroApp/AppDelegate.swift
+++ PomodoroApp/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        // Initialize the Google Mobile Ads SDK.
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
         return true
     }
 
