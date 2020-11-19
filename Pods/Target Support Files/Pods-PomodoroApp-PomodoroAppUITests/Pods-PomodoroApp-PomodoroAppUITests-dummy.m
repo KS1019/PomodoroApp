d78624127f447059b7eb7349b4ab7818528bdd38
KOTARO SUTO <ng1a5ss43@gmail.com>
2020-11-20 05:49:40 +0900

    Run 'pod install'

---
+++ Pods/Target Support Files/Pods-PomodoroApp-PomodoroAppUITests/Pods-PomodoroApp-PomodoroAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PomodoroApp_PomodoroAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PomodoroApp_PomodoroAppUITests
+@end
---
+++ Pods/Target Support Files/Pods-PomodoroAppTests/Pods-PomodoroAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PomodoroAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_PomodoroAppTests
+@end
